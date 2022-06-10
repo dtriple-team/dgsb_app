05a5713d2c836b120af330521a2c66d93c05066b
jjaekkaemi <21600752@handong.edu>
2022-06-10 19:36:48 +0900

    edit error, add write

--- ble.py
+++ ble.py
@@ -1,13 +1,14 @@
 import asyncio
 from bleak import BleakClient, BleakScanner
 import protocol
-import threading, time
+import threading, time, copy
+read_write_charcteristic_uuid = "f0001111-0451-4000-b000-000000000000"
 class BLE:
     def __init__(self):
-        self.loop = asyncio.get_event_loop()
+        self.loop = None
 
         self.scanner = None
-        self.is_scanning = None
+        self.is_scanning = False
 
         self.devicelist = None
         self.select_address = None
@@ -15,7 +16,8 @@ class BLE:
         self.root = None
         self.connected_client = []
 
-        self.running = True
+        self.is_running = False
+
     def root_connect(self, root):
         self.root = root
 
@@ -24,28 +26,78 @@ class BLE:
         self.devicelist = []
         self.select_address = None
 
+    def do_asyncio_tasks(self):
+        self.loop = asyncio.new_event_loop()
+        threading.Thread(target=self.asyncio_thread).start()
+    def do_asyncio_stop_tasks(self):
+        threading.Thread(target=self.asyncio_stop_thread, args=(False,)).start()
+    def do_asyncio_close_tasks(self):
+        return threading.Thread(target=self.asyncio_stop_thread, args=(True,))
     def do_scan_tasks(self):
         print("[BTN EVENT] SCAN START")
         threading.Thread(target=self.asyncio_scan_thread).start()
-
+    def do_scan_stop_tasks(self):
+        print("[BTN EVENT] SCAN STOP")
+        threading.Thread(target=self.asyncio_scan_stop_thread).start()
     def do_ble_connect_tasks(self):
         print("[BTN EVENT] BLE CONNECT")
         threading.Thread(target=self.asyncio_ble_connect_thread).start()
+    
+    def do_ble_disconnect_tasks(self):
+        print("[BTN EVENT] BLE DISCONNECT")
+        threading.Thread(target=self.asyncio_ble_disconnect_thread).start()
+    
+    def do_ble_write_tasks(self, packet):
+        threading.Thread(target=self.asyncio_ble_write_thread, args=(packet,)).start()
+
+    def asyncio_thread(self):
+        self.loop.run_until_complete(self.asyncio_start())
+        self.loop.close()
+        if not self.root.get_close_status() :
+            self.root.change_ui(self.is_running)
+
+    def asyncio_stop_thread(self, is_close):
+        if self.is_running:
+            self.loop.create_task(self.asyncio_stop(is_close))
+        else :
+            print("비동기 루프 실행 상태가 아닙니다.")
 
     def asyncio_scan_thread(self):
-        self.loop.run_until_complete(self.ble_scan())
-        # if self.select_address:
-        #     self.loop.run_until_complete(self.ble_connect())
+        self.loop.create_task(self.ble_scan())
+
+    def asyncio_scan_stop_thread(self):
+        self.loop.create_task(self.ble_scan_stop())
 
     def asyncio_ble_connect_thread(self):
-        self.select_address = self.root.devicelistbox_return()
-        print()
-        if self.select_address :
-            self.ble_scan_stop()
-        else :
+        address = self.root.devicelistbox_return()
+        if self.is_click_device(address) :
+            self.loop.create_task(self.ble_connect(address))
+        
+    def asyncio_ble_disconnect_thread(self):
+        address = self.root.clientlistbox_return()
+        if self.is_click_device(address):
+            self.loop.create_task(self.ble_disconnect(address))   
+
+    def asyncio_ble_write_thread(self, packet):
+        address = self.root.clientlistbox_return()
+        if self.is_click_device(address):
+            index = self.get_index_select_client(address.split(' ')[1])
+            if index >= 0 :
+                print("ble_write_check")
+                self.loop.create_task(self.ble_write_check(index, packet))
+
+    def is_click_device(self, address):
+        if address:
+            return True
+        else:
             print("Please select ble device !")
-        self.loop.create_task(self.ble_connect(self.select_address))
-           
+            return False
+    def get_index_select_client(self, address):
+        for i in range(len(self.connected_client)):
+            if self.connected_client[i].address == address:
+                return i
+        print("Disconnected Device !")
+        return None
     def detection_callback(self, device, advertisement_data):
         if device.name is not None:
             device_info = f'{device.name} {device.address}'
@@ -53,26 +105,47 @@ class BLE:
                 self.devicelist.append(device_info)
                 self.root.devicelist_insert(len(self.devicelist)-1, device_info)
 
-    async def ble_scan(self):
-        self.scan_init()
-        self.scanner = BleakScanner()
-        self.scanner.register_detection_callback(self.detection_callback)
-        await self.scanner.start()
-        self.root.status_label_set("STATUS : Scanning..")
-        self.scanning = True
-        while(self.scanning):
-            await asyncio.sleep(1)
-        await self.scanner.stop()    
-        self.root.status_label_set("STATUS : Scan Stop")
-        while self.running:
-            await asyncio.sleep(1)
-    def ble_scan_stop(self):
-        print("[BTN EVENT] SCAN STOP")
-        self.scanning = False
+    def get_is_running(self):
+        return self.is_running
 
+    async def asyncio_start(self):
+        if not self.is_running: 
+            self.is_running = True
+            self.root.change_ui(self.is_running)
+            while self.is_running:
+                await asyncio.sleep(1)
+        else :
+            print("비동기 루프를 실행중입니다.")
+
+    async def asyncio_stop(self, is_close):
+        await self.ble_scan_stop(is_close)
+        await self.ble_all_disconnect(is_close)
+        await asyncio.sleep(2)
+        self.is_running = False
+
+
+    async def ble_scan(self):
+        if not self.is_scanning : 
+            self.scan_init()
+            self.scanner = BleakScanner()
+            self.scanner.register_detection_callback(self.detection_callback)
+            await self.scanner.start()
+            self.root.status_label_set("STATUS : Scanning..")
+            self.is_scanning = True
+        else :
+            print("스캔 상태 입니다.")
+
+    async def ble_scan_stop(self, is_close):
+        if self.is_scanning:
+            await self.scanner.stop()  
+            self.is_scanning = False  
+            if not is_close:
+                self.root.status_label_set("STATUS : Scan Stop")
+        else :
+            print("스캔 종료 상태 입니다.")
     def on_disconnect(self, client):
         print("[BLE CALLBACK] Client with address {} got disconnected!".format(client.address))
-        self.root.status_label_set(f"STATUS : Disonnected "+client.address)
+        self.root.clientlistbox_find_delete(client.address)
         for i in range(len(self.connected_client)) :
             if self.connected_client[i].address == client.address:
                 del self.connected_client[i]
@@ -80,17 +153,43 @@ class BLE:
 
     async def ble_connect(self, address):
         self.root.status_label_set("STATUS : Connecting..")
-        print(address)
-        self.connected_client.append(BleakClient(address))
+        self.connected_client.append(BleakClient(address.split(' ')[1]))
         client = self.connected_client[len(self.connected_client)-1]
         try:
             client.set_disconnected_callback(self.on_disconnect)
-           
             await client.connect()
+            self.root.clientlistbox_insert(len(self.connected_client)-1, address)
             self.root.status_label_set(f"STATUS : Connected "+client.address)
         except Exception as e:
             print('[ERR] : ', e) 
         finally:
             await asyncio.sleep(1) 
-        print("[BLE] Disconnect")
 
+    async def ble_disconnect(self, address):
+        for i in range(len(self.connected_client)) :
+            if self.connected_client[i].address == address.split(' ')[1]:
+                
+                await asyncio.sleep(1)
+                await self.connected_client[i].disconnect()
+                break
+    async def ble_all_disconnect(self):
+        temp_list = self.connected_client.copy()
+        for i in range(len(temp_list)) :
+            await asyncio.sleep(1)
+            await temp_list[i].disconnect()
+            
+    async def ble_write_check(self, index, data):
+        if len(data)>=17:
+            await self.ble_write(index, data[0:16])
+            await self.ble_write_check(data[16:len(data)])
+        else:
+            await self.ble_write(index, data[0:16])
+        
+    async def ble_write(self, i, data):
+        self.root.write_label_set(data)
+        await asyncio.sleep(1)
+        print("[BLE] WRITE ", data)
+        await self.connected_client[i].write_gatt_char(read_write_charcteristic_uuid,  data)
+        
+    
+    
--- tkgui.py
+++ tkgui.py
@@ -1,22 +1,29 @@
 from tkinter import Tk, Label, Button, StringVar, constants, Frame, Listbox, Scrollbar, Text
 import ble as b
+import time,sys 
+import threading
 class TkGUI:
     def __init__(self):
         self.root = Tk()
         self.loop = b.BLE()
         self.root.title("GUI TEST")
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.geometry("1080x600")
+        self.is_close = False
+        self.ble_start_btn = Button(self.root, text="BLE START", command=self.loop.do_asyncio_tasks)
+        self.ble_stop_btn = Button(self.root, text="BLE STOP", command=self.loop.do_asyncio_stop_tasks)
+        
         self.status_text = StringVar()
         self.status_text.set("STATUS : IDLE")
         self.status_label = Label(self.root, textvariable=self.status_text)
-        self.status_label.pack()
+        
         
         self.frame_scan = Frame(self.root)
         self.frame_scan.pack(side="left", fill="both", expand=True)
         
         
         self.scan_btn = Button(self.frame_scan, text="BLE Scan", command=self.loop.do_scan_tasks)
-        self.scan_stop_btn = Button(self.frame_scan, text="BLE Scan Stop", command=self.loop.ble_scan_stop)
+        self.scan_stop_btn = Button(self.frame_scan, text="BLE Scan Stop", command=self.loop.do_scan_stop_tasks)
         self.connect_btn = Button(self.frame_scan, text="BLE Connect", command=self.loop.do_ble_connect_tasks)
         self.frame = Frame(self.frame_scan)
         self.scrollbar = Scrollbar(self.frame, orient="vertical")
@@ -25,9 +32,9 @@ class TkGUI:
         # # -----------------------------------------------------------------------------------------
         self.frame_connect = Frame(self.root)
         self.frame_connect.pack(side="right", fill="both", expand=True)
-        self.close_btn = Button(self.frame_connect, text="BLE Disconnect")
+        self.close_btn = Button(self.frame_connect, text="BLE Disconnect", command=self.loop.do_ble_disconnect_tasks)
 
-        self.measure_btn = Button(self.frame_connect, text="Measure Start")
+        self.measure_btn = Button(self.frame_connect, text="Measure Start", command=lambda:self.loop.do_ble_write_tasks(bytearray([0x02, 0x03])))
         self.stop_btn = Button(self.frame_connect, text="Measure Stop")
         self.get_hr_btn = Button(self.frame_connect, text="GET HR")
         self.get_spo2_btn = Button(self.frame_connect, text="GET SPO2")
@@ -38,7 +45,7 @@ class TkGUI:
         self.read_text.set("READ DATA : ")
 
         self.write_text = StringVar()
-        self.write_text.set("READ DATA : ")   
+        self.write_text.set("WRITE DATA : ")   
 
         self.write_label=Label(self.frame_connect, textvariable=self.write_text)
         self.read_label=Label(self.frame_connect, textvariable=self.read_text)
@@ -51,15 +58,44 @@ class TkGUI:
         
         self.text=Text(self.frame_connect)
         self.submit_btn = Button(self.frame_connect, text="Submit")
-
-
-
+        
         self.loop.root_connect(self)
-
+        
+        self.change_ui(False)
     def run(self):
         # self.change_ui(False)
         self.root.mainloop()
 
+    def on_closing(self):
+        self.is_close = True
+        t1 = self.loop.do_asyncio_close_tasks()
+        t2 = threading.Thread(target=self.close)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+    def get_close_status(self):
+        return self.is_close
+    def close(self):
+        print("종료 !!")
+        while self.loop.is_running:
+            print("..?")
+            time.sleep(2)
+        self.root.destroy() 
+    def change_ui(self, is_running):
+        if is_running:
+            self.ble_start_btn.pack_forget()
+            self.ble_stop_btn.pack(side="bottom", fill="x")
+            self.status_label.pack(side="top", fill="x")
+            self.before_connect_show()
+            self.after_connect_show()
+            
+        else:
+            self.ble_stop_btn.pack_forget()
+            self.status_label.pack_forget()
+            self.before_connect_hide()
+            self.after_connect_hide()
+            self.ble_start_btn.pack(side="top", fill="x")
     def before_connect_show(self):
 
         self.scan_btn.grid(row=1, column=0, sticky=constants.W)
@@ -72,12 +108,12 @@ class TkGUI:
 
     def before_connect_hide(self):
 
-        self.scan_btn.grid(row=1, column=0, sticky=constants.W)
-        self.scan_stop_btn.grid(row=1, column=1, sticky=constants.W)
-        self.connect_btn.grid(row=1, column=2, sticky=constants.W)
-        self.frame.grid(row=2, column=0, columnspan=3, sticky=constants.W+constants.E+constants.N+constants.S )
-        self.scrollbar.pack(side=constants.RIGHT, fill=constants.Y)
-        self.devicelistbox.pack(expand=True, fill=constants.Y)
+        self.scan_btn.grid_forget()
+        self.scan_stop_btn.grid_forget()
+        self.connect_btn.grid_forget()
+        self.frame.grid_forget()
+        self.scrollbar.pack_forget()
+        self.devicelistbox.pack_forget()
         
     def after_connect_show(self):
 
@@ -115,24 +151,15 @@ class TkGUI:
         self.label.grid_forget()
         self.text.grid_forget()
         self.submit_btn.grid_forget()
-        
-    def change_ui(self, is_connected):
-        if is_connected:
-            self.before_connect_hide()
-            self.after_connect_show()
-        else :
-            self.after_connect_hide()
-            self.before_connect_show()
-            
 
     def status_label_set(self, messeage):
         self.status_text.set(messeage)
 
     def read_label_set(self, data):
-        self.read_text.set(f"WRITE DATA : {data}")
+        self.read_text.set(f"READ DATA : {data}")
 
     def write_label_set(self, data):
-        self.write_text.set(f"READ DATA : {data}")
+        self.write_text.set(f"WRITE DATA : {data}")
 
     def devicelistbox_init(self):
         self.devicelistbox.delete(0, constants.END)
@@ -142,9 +169,39 @@ class TkGUI:
 
     def devicelistbox_return(self):
         for i in self.devicelistbox.curselection():
-            return self.devicelistbox.get(i).split(' ')[1]
+            return self.devicelistbox.get(i)
         return None
+
+    def devicelistbox_selete_delete(self):
+        for i in self.devicelistbox.curselection():
+            self.devicelistbox.delete(i)
+            break
+
+
+    def clientlistbox_insert(self, num, info):
+        self.client_listbox.insert(num, info)
+
+    def clientlistbox_return(self):
+        for i in self.client_listbox.curselection():
+            return self.client_listbox.get(i)
+        return None
+
+    def clientlistbox_index_return(self):
+        for i in self.client_listbox.curselection():
+            return i
+    def clientlistbox_selete_delete(self):
+        self.client_listbox.delete(self.clientlistbox_index_return())
+        
+    def clientlistbox_index_delete(self, index):
+        self.client_listbox.delete(index)
+
+    def clientlistbox_find_delete(self, address):
+        for i in range(self.client_listbox.size()):
+            if address in self.client_listbox.get(i):
+                self.clientlistbox_index_delete(i)
+                break
+    def clientlistbox_all_delete(self):
+        self.client_listbox.delete(0, constants.END)
 my_gui = TkGUI()
-my_gui.before_connect_show()
-my_gui.after_connect_show()
 my_gui.run()
+
